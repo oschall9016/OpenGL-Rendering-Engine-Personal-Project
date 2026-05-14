9118723cd5db5b5964ca2cf605686d75159cbcf5
schall650 <skyguy6501@gmail.com>
2026-05-14 16:51:02 -0600

    commenting out for testing

--- include/Mesh.h
+++ include/Mesh.h
@@ -14,21 +14,23 @@ struct Vertex
 	glm::vec2 TexCoords;
 };
 
+/*
 struct Texture
 {
 	std::string path;
 	std::string type;
 	unsigned int id;
 };
+*/
 
 class Mesh
 {
 public:
 	std::vector<Vertex>       vertices;
 	std::vector<unsigned int> indices;
-	std::vector<Texture>      textures;
+	//std::vector<Texture>      textures;
 
-	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices/*, std::vector<Texture> textures*/);
 	void Draw(Shader &shader);
 
 private:
