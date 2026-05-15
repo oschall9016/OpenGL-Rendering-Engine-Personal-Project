8b6d202a50cf3eb304aa9e5d9f763068463360a8
oschall9016 <oschall9016@gmail.com>
2026-05-14 18:26:55 -0600

    now loads textures and models

---
+++ src/AssetManager.cpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "AssetManager.h"
+
+#include "Texture.h"
+#include "Model.h"
+#include "NewModelLoader.h"
+
+#include <memory>
+#include <map>
+#include <string>
+#include <iostream>
+
+std::shared_ptr<Texture> AssetManager::LoadTexture(const std::string& path)
+{
+	// if texture is in the list
+	if (Textures.find(path) != Textures.end())
+	{
+		std::cout << "Texture: " << path << " Is Already Loaded \n";
+		return Textures.at(path);
+	}
+
+	// texture not in list, needs loaded
+	Textures.insert({path,NewModelLoader::LoadTexture(path)});
+	return Textures.at(path);
+}
+
+std::shared_ptr<Model> AssetManager::LoadModel(const std::string& path)
+{
+	// if texture is in the list
+	if (Models.find(path) != Models.end())
+	{
+		std::cout << "Model: " << path << " Is Already Loaded \n";
+		return Models.at(path);
+	}
+
+	// texture not in list, needs loaded
+	Models.insert({ path,NewModelLoader::LoadModel(path) });
+	return Models.at(path); 
+}
+
+
+
+
