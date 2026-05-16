d9e16b36c66426934afa458f0557f45879ddf2cd
oschall9016 <oschall9016@gmail.com>
2026-05-16 00:10:55 -0600

    made class variables public

--- include/Texture.h
+++ include/Texture.h
@@ -4,11 +4,15 @@
 
 class Texture
 {
+	// TODO write getters
 public:
 	Texture(std::string path,unsigned int ID, int width, int height);
 	~Texture();
-private:
+
 	unsigned int ID;
 	int width, height;
 	std::string path;
+
+
+
 };
