01dbc5a60d0394441708e2679a9789bced278eec
schall650 <skyguy6501@gmail.com>
2026-05-17 20:26:55 -0600

    split assetimporter into seperate model and texture importers

---
+++ include/TextureImporter.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Texture.h"
+
+#include "AssetManager.h"
+
+#include <memory>
+
+#include <assimp/scene.h>
+
+class TextureImporter
+{
+public:
+	static std::shared_ptr<Texture> ImportTexture(const std::string& path, AssetManager& manager);
+};
