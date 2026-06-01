43a3b1c4896007c4e10d1556d3fe7e3e55f980c0
oschall9016 <oschall9016@gmail.com>
2026-05-31 18:19:58 -0600

    added shaders to the assetmanager

--- include/AssetManager.h
+++ include/AssetManager.h
@@ -2,6 +2,7 @@
 
 #include "Texture.h"
 #include "Model.h"
+#include "Shader.h"
 
 #include <memory>
 #include <unordered_map>
@@ -12,7 +13,9 @@ class AssetManager
 public:
 	std::shared_ptr<Texture> LoadTexture(const std::string& path);
 	std::shared_ptr<Model> LoadModel(const std::string& path);
+	std::shared_ptr<Shader> LoadShader(const std::string& name, const std::string& vPath, const std::string& fPath);
 private:
 	std::unordered_map<std::string, std::shared_ptr<Texture>> Textures;
 	std::unordered_map<std::string, std::shared_ptr<Model>> Models;
+	std::unordered_map<std::string, std::shared_ptr<Shader>> Shaders;
 };
