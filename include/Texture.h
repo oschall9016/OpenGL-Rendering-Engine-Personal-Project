0b0e87c9ad20c84544134c3b09cec990efe5a18d
oschall9016 <oschall9016@gmail.com>
2026-06-06 22:08:08 -0600

    added getter functions for class members

--- include/Texture.h
+++ include/Texture.h
@@ -9,10 +9,15 @@ public:
 	Texture(std::string path,unsigned int ID, int width, int height);
 	~Texture();
 
+	const unsigned int& GetID();
+	const int& GetWidth();
+	const int& GetHeight();
+	const std::string& GetPath();
+	
+private:
 	unsigned int ID;
 	int width, height;
 	std::string path;
-
 
 
 };
