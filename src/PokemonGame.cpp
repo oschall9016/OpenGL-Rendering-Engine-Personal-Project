5243575ecabe711e518bc3e6d44ddcf9b6d2d5ae
schall650 <skyguy6501@gmail.com>
2026-06-15 15:50:29 -0600

    edited main file to test components and systems

--- src/PokemonGame.cpp
+++ src/PokemonGame.cpp
@@ -6,9 +6,17 @@
 
 #include "Mesh.h"
 
-#include "EntityHandler.h"
-
 #include "AssetManager.h"
+
+#include "Renderer.h"
+
+#include "TestScene.h"
+#include "c_Renderable.h"
+#include "s_Render.h"
+#include "Entity.h"
+#include "EntityHandler.h"
+#include "SparseSet.h"
+#include "ComponentManager.h"
 
 #include <iostream>
 #include <memory>
@@ -45,9 +53,25 @@ int main(int argc, char* args[])
     float lastFrame = 0.0f;
     
     AssetManager aManager;
+    Renderer renderer;
 
     std::shared_ptr<Model> backpack = aManager.LoadModel("assets/models/backpack/backpack.obj");
     std::shared_ptr<Model> backpack2 = aManager.LoadModel("assets/models/backpack/backpack.obj");
+
+    int maxEntities = 5;
+    EntityHandler entityHandler(maxEntities);
+    ComponentManager componentManager(maxEntities);
+    componentManager.RegisterComponent<c_Renderable>();
+
+    s_Render renderSystem(renderer, componentManager);
+
+    for (int i = 0; i < maxEntities; i++)
+    {
+        Entity entity = entityHandler.CreateEntity();
+        c_Renderable entityRenderData = c_Renderable{ aManager.LoadModel("assets/models/backpack/backpack.obj"), aManager.LoadShader("BackpackShader","assets/shaders/BasicBackpackVertex.vs", "assets/shaders/BasicBackpackFragment.fs")};
+        renderSystem.entities.insert(entity);
+        componentManager.AddComponent(entity, entityRenderData);
+    }
 
     // main game loop
     bool gameRunning = true;
@@ -62,16 +86,16 @@ int main(int argc, char* args[])
 
         basicShader.use(); 
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        //glm::mat4 model = glm::mat4(1.0f);
+        //model = glm::rotate(model, glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
         glm::mat4 view = camera.createViewMatrix();
 
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-        int modelLoc = glGetUniformLocation(basicShader.getID(), "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        //int modelLoc = glGetUniformLocation(basicShader.getID(), "model");
+        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
         int viewLoc = glGetUniformLocation(basicShader.getID(), "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
@@ -79,13 +103,15 @@ int main(int argc, char* args[])
         int projLoc = glGetUniformLocation(basicShader.getID(), "projection");
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-        backpack->Draw(basicShader);
+        //backpack->Draw(basicShader);
+        //renderer.RenderModel(*backpack, basicShader);
 
-        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        modelLoc = glGetUniformLocation(basicShader.getID(), "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        //model = glm::rotate(model, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        //modelLoc = glGetUniformLocation(basicShader.getID(), "model");
+        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-        backpack2->Draw(basicShader);
+        //backpack2->Draw(basicShader);
+        renderSystem.RenderEntitites();
 
         input.updateKeyState();
         camera.ProcessInput(input, deltaTime);
