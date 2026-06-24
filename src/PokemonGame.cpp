44a56b427177f3d933695a2e3281d81f631c4789
oschall9016 <oschall9016@gmail.com>
2026-06-23 18:06:19 -0600

    changed camera to use default initialization and added back backpack drawing for testing

--- src/PokemonGame.cpp
+++ src/PokemonGame.cpp
@@ -32,11 +32,7 @@ int main(int argc, char* args[])
 
     SDLWindow window(800, 600, "Cool Window");
 
-    glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f);
-    glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-
-    Camera camera(position, front, up);
+    Camera camera;
     SDLInput input;
 
     float currentFrame = 0.0f;
@@ -46,34 +42,31 @@ int main(int argc, char* args[])
     AssetManager aManager;
     Renderer renderer;
 
-    int maxEntities = 5;
+    int maxEntities = 2;
     EntityComponentSystem ecs;
 
     ecs.RegisterComponent<c_Renderable>();
 
     s_Render renderSystem(renderer, ecs, camera);
-
-    /*
+ 
     std::shared_ptr<Shader> basicShader = aManager.LoadShader("BackpackShader", "assets/shaders/BasicBackpackVertex.vs", "assets/shaders/BasicBackpackFragment.fs");
     std::shared_ptr<Model> backpackModel = aManager.LoadModel("assets/models/backpack/backpack.obj");
-    */
-
+   
     std::shared_ptr<Texture> testSpriteTexture = aManager.LoadTexture("assets/sprites/HeroSpriteTestTrimmed.png");
     std::shared_ptr<Shader> testSpriteShader = aManager.LoadShader("SpriteShader", "assets/shaders/spriteVertex.vs", "assets/shaders/spriteFragment.fs");
     std::shared_ptr<Model> testSpriteModel = aManager.LoadModel("SpriteModel", Model::CreateQuad(testSpriteTexture));
 
-    /*
-    for (int i = 0; i < maxEntities; i++)
-    {
-        Entity entity = ecs.CreateEntity();
-        c_Renderable entityRenderData = c_Renderable{ backpackModel, basicShader };
-        ecs.AddComponent<c_Renderable>(entity,entityRenderData);
-    }
-    */
-
     Entity spriteEntity = ecs.CreateEntity();
     c_Renderable spriteRenderData = c_Renderable{testSpriteModel, testSpriteShader};
     ecs.AddComponent<c_Renderable>(spriteEntity, spriteRenderData);
+
+   for (int i = 0; i < maxEntities; i++)
+   {
+       Entity entity = ecs.CreateEntity();
+       c_Renderable entityRenderData = c_Renderable{ backpackModel, basicShader };
+       ecs.AddComponent<c_Renderable>(entity,entityRenderData);
+   }
+   
 
     // main game loop
     bool gameRunning = true;
@@ -86,7 +79,7 @@ int main(int argc, char* args[])
         glClearColor(0.1f, 0.6f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        renderSystem.RenderEntitites(); // TODO: check if model still has its own draw function
+        renderSystem.RenderEntitites();
 
         input.updateKeyState();
         camera.ProcessInput(input, deltaTime);
