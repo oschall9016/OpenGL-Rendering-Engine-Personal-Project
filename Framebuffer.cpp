9e8b8b14da4d72418493a15f165d45e6a7c95a0b
oschall9016 <oschall9016@gmail.com>
2026-06-25 21:51:49 -0600

    created framebuffer class for post processing effects

---
+++ Framebuffer.cpp
@@ -0,0 +1,115 @@
+#include "Framebuffer.h"
+#include "Shader.h"
+
+#include <glad/glad.h>
+
+#include <iostream>
+#include <memory>
+
+Framebuffer::Framebuffer(int width, int height)
+{
+	this->width = width;
+	this->height = height;
+
+	// create fullscreen quad
+	quadVAO = quickQuad();
+
+	// generate framebuffer
+	glGenFramebuffers(1, &framebufferID);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebufferID);
+
+	// generate textureColorBuffer
+	glGenTextures(1, &textureColorbufferID);
+	glBindTexture(GL_TEXTURE_2D, textureColorbufferID);
+
+	// create texture?
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbufferID, 0);
+
+	// create renderbuffer object for depth
+	unsigned int rbo;
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height); // what is GL_DEPTH24_STENCIl8?
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!\n";
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+Framebuffer::~Framebuffer()
+{
+	glDeleteFramebuffers(1, &framebufferID); // TODO: make sure im deleting other objects when i need to
+}
+
+void Framebuffer::Bind()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, framebufferID);
+	glViewport(0, 0, width, height);
+}
+
+void Framebuffer::Unbind(int screenWidth, int screenHeight)
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glViewport(0, 0, screenWidth, screenHeight);
+}
+
+void Framebuffer::renderFramebufferQuad(std::shared_ptr<Shader> shader)
+{
+
+	glDisable(GL_DEPTH_TEST); // makes sure quad isn't effected by depth test
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	shader->use();
+
+	// draw the quad
+	glBindVertexArray(quadVAO);
+	glBindTexture(GL_TEXTURE_2D, textureColorbufferID);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glEnable(GL_DEPTH_TEST);
+}
+
+unsigned int Framebuffer::quickQuad()
+{
+	float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+		// positions   // texCoords
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f,  0.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f
+	};
+	unsigned int quadVAO, quadVBO;
+	glGenVertexArrays(1, &quadVAO);
+	glGenBuffers(1, &quadVBO);
+	glBindVertexArray(quadVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+	return quadVAO;
+
+}
+
+const unsigned int& Framebuffer::GetFramebufferID()
+{
+	return framebufferID;
+}
+
+const unsigned int& Framebuffer::GetTextureColorbufferID()
+{
+	return textureColorbufferID;
+}
--- include/Framebuffer.h
+++ include/Framebuffer.h
@@ -1,19 +1,29 @@
 #pragma once
 
+#include "Shader.h"
+#include <memory>
+
 class Framebuffer
 {
 public:
 	Framebuffer(int width, int height);
+	~Framebuffer();
 
 	void Bind();
-	void Unbind();
+	void Unbind(int screenWidth, int screenHeight);
 
-	const unsigned int& GetFrameBufferID();
-	const unsigned int& GetTextureColorBufferID();
+	void renderFramebufferQuad(std::shared_ptr<Shader> shader);
+	unsigned int quickQuad();
+
+	const unsigned int& GetFramebufferID();
+	const unsigned int& GetTextureColorbufferID();
 
 private:
-	unsigned int frameBufferID;
-	unsigned int textureColorBufferID;
+	unsigned int framebufferID;
+	unsigned int textureColorbufferID;
+
+	unsigned int quadVAO;
+
 	int width;
 	int height;
 };
