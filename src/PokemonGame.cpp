fdd64d785f597dfe9d4feea329517e885b1a70e2
oschall9016 <oschall9016@gmail.com>
2026-07-07 15:39:07 -0600

    edited test file to use new ecs system manager

--- src/PokemonGame.cpp
+++ src/PokemonGame.cpp
@@ -36,8 +36,8 @@ int main(int argc, char* args[])
     int windowWidth = 1920;
     int windowHeight = 1080;
 
-    int renderWidth = 256; //256
-    int renderHeight = 144; //144
+    float renderWidth = 256; //256
+    float renderHeight = 144; //144
 
     SDLWindow window(windowWidth, windowHeight, "Cool Window");
 
@@ -56,31 +56,35 @@ int main(int argc, char* args[])
 
     ecs.RegisterComponent<c_Renderable>();
 
-    s_Render renderSystem(renderer, ecs, camera);
+    auto renderSystem = std::make_shared<s_Render>(renderer, ecs, camera);
+    ecs.RegisterSystem<s_Render, c_Renderable>(renderSystem);
  
+    /////////////////////// Asset Loading //////////////////////////
     std::shared_ptr<Shader> basicShader = aManager.LoadShader("BackpackShader", "assets/shaders/BasicBackpackVertex.vs", "assets/shaders/BasicBackpackFragment.fs");
     
-    //std::shared_ptr<Model> backpackModel = aManager.LoadModel("assets/models/testmodel2/Test Model.obj"); //"assets/models/backpack/backpack.obj"
-    std::shared_ptr<Model> backpackModel = aManager.LoadModel("assets/models/backpack/backpack.obj");   
+    std::shared_ptr<Model> backpackModel = aManager.LoadModel("assets/models/testmodel2/Test Model.obj"); //"assets/models/backpack/backpack.obj"
+    //std::shared_ptr<Model> backpackModel = aManager.LoadModel("assets/models/backpack/backpack.obj");   
 
-    std::shared_ptr<Texture> testSpriteTexture = aManager.LoadTexture("assets/sprites/HeroSpriteTestTrimmed.png");
+    std::shared_ptr<Texture> testSpriteTexture = aManager.LoadTexture("assets/sprites/HeroSpriteTestTrimmed.png", true);
     std::shared_ptr<Shader> testSpriteShader = aManager.LoadShader("SpriteShader", "assets/shaders/spriteVertex.vs", "assets/shaders/spriteFragment.fs");
     std::shared_ptr<Model> testSpriteModel = aManager.LoadModel("SpriteModel", Model::CreateQuad(testSpriteTexture));
 
     std::shared_ptr<Shader> framebufferShader = aManager.LoadShader("FramebufferShader", "assets/shaders/FramebufferVertex.vs", "assets/shaders/FramebufferFragment.fs");
+    /////////////////////// //////////// ///////////////////////////
 
-    Entity spriteEntity = ecs.CreateEntity();
+
+    Entity spriteEntity = ecs.CreateEntity(); // 0
     c_Renderable spriteRenderData = c_Renderable{testSpriteModel, testSpriteShader};
     ecs.AddComponent<c_Renderable>(spriteEntity, spriteRenderData);
 
-   for (int i = 0; i < maxEntities; i++)
-   {
-       Entity entity = ecs.CreateEntity();
-       c_Renderable entityRenderData = c_Renderable{ backpackModel, basicShader };
-       ecs.AddComponent<c_Renderable>(entity,entityRenderData);
-   }
+    for (int i = 0; i < maxEntities; i++)
+    {
+        Entity entity = ecs.CreateEntity(); // 1 2 
+        c_Renderable entityRenderData = c_Renderable{ backpackModel, basicShader };
+        ecs.AddComponent<c_Renderable>(entity,entityRenderData);
+    }
    
-   Framebuffer pixelFramebuffer(renderWidth, renderHeight);
+    Framebuffer pixelFramebuffer(renderWidth, renderHeight);
 
     // main game loop
     bool gameRunning = true;
@@ -93,16 +97,16 @@ int main(int argc, char* args[])
         glClearColor(0.1f, 0.6f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        renderSystem.RenderEntitites();
+        renderSystem->RenderEntitites();
 
         pixelFramebuffer.Unbind(windowWidth,windowHeight);
 
         pixelFramebuffer.renderFramebufferQuad(framebufferShader);
 
         input.updateKeyState();
+
         camera.ProcessInput(input, dt.Get());
         camera.ProcessMouse(input.getMouseX(), input.getMouseY());
-
 
         window.SwapBuffers();
         
