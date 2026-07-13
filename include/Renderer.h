d722ade5852856cf67c3df66d78e5f94a8cce561
schall650 <skyguy6501@gmail.com>
2026-07-13 17:04:14 -0600

    added functions for rendering framebuffer quads and skyboxs

--- include/Renderer.h
+++ include/Renderer.h
@@ -2,13 +2,23 @@
 
 #include "Model.h"
 #include "Shader.h"
+#include "Framebuffer.h"
+#include "Skybox.h"
+#include "Camera.h"
+
+#include <memory>
 
 class Renderer
 {
 public:
 	Renderer();
+
 	void RenderModel(Model& model, Shader& shader);
 	void RenderMesh(Mesh& mesh, Shader& shader);
+
+	void RenderFramebufferQuad(Framebuffer& framebuffer, Shader& shader);
+	void RenderSkybox(Skybox& skybox, Shader& shader, Camera& camera);
+
 
 private:
 	
