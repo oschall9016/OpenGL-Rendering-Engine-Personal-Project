4f2b74cc9c016e1e57df2a68e46a4d7345123b0f
schall650 <oschall9016@gmail.com>
2026-08-27 16:28:36 -0600

    removed old movment pipeline files, added new movement pipline component and system files, and changed location of SpriteAnimation.cpp

---
+++ PokemonGameTest/src/animations/SpriteAnimation.cpp
@@ -0,0 +1,23 @@
+#include "SpriteAnimation.h"
+
+const int SpriteAnimation::GetCurrentFrame()
+{
+	return frames[currFrameIndex];
+}
+
+void SpriteAnimation::SetCurrentFrame(int frame)
+{
+	currFrameIndex = frame;
+}
+
+void SpriteAnimation::Update()
+{
+	if (currFrameIndex + 1 >= frames.size())
+	{
+		currFrameIndex = 0;
+	}
+	else
+	{	
+		currFrameIndex++;
+	}
+}
