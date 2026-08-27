16e00671427a1af481847cbfeb1c762a43733fa6
schall650 <oschall9016@gmail.com>
2026-08-27 16:02:51 -0600

    created a current state component to store player input and movement info

---
+++ PokemonGameTest/include/components/component_player_currentState.h
@@ -0,0 +1,49 @@
+#pragma once
+
+// enum values are used for uv sprite animations
+// uv row = Action
+// uv col = Direction + Frame
+
+// determines the row of the sprite sheet
+enum class Input_Action
+{
+	STANDING = -1,
+	WALKING = 0,
+	RUNNING = 1
+};
+
+// determines the column of the sprite sheet
+enum class Input_Direction
+{
+	UP = 0,
+	DOWN = 3,
+	LEFT = 6,
+	RIGHT = 9
+};
+
+enum class Key_State
+{
+	PRESSED,
+	HELD,
+	RELEASED
+};
+
+// time (in seconds) it takes to move one tile
+constexpr float WALK_SPEED = 0.4f; 
+constexpr float RUN_SPEED = 0.25f; 
+constexpr float BIKE_SPEED = 0.1f; 
+
+struct component_player_currentState
+{
+	Input_Action currentAction = Input_Action::STANDING;
+	Input_Direction currentDirection = Input_Direction::DOWN;
+
+	Input_Direction spriteDirection = Input_Direction::DOWN;
+
+	Key_State keyState = Key_State::RELEASED;
+
+	bool moveRequest = false;
+	bool movementVisualsSynced = true;
+
+	float moveSpeed = WALK_SPEED; 
+};
