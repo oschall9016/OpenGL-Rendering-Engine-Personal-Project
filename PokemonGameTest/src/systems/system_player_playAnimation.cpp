9235fa539b05e1ddefa4d2744707ba85b23f07bc
schall650 <oschall9016@gmail.com>
2026-08-28 18:26:10 -0600

    removed commented out code and unbinded shader after use

--- PokemonGameTest/src/systems/system_player_playAnimation.cpp
+++ PokemonGameTest/src/systems/system_player_playAnimation.cpp
@@ -11,55 +11,10 @@
 *   - component_Model
 */
 
-system_player_playAnimation::system_player_playAnimation(EntityComponentSystem& ecs) : ecs(ecs) {}
-
 // uv row = Action
 // uv col = Direction + Frame
 
-/*
-void system_player_playAnimation::Update()
-{
-	for (auto& entity : entities)
-	{
-		auto currentState = ecs.GetComponent<component_player_currentState>(entity);
-		auto shader = ecs.GetComponent<component_Model>(entity)->shader;
-		auto animation = ecs.GetComponent<component_player_animations>(entity);
-
-		
-		// cast to int for uv calculating
-		int currentAction = static_cast<int>(currentState->currentAction);
-		int currentDirection = static_cast<int>(currentState->currentDirection);
-
-		int uvRow = 0;
-		int uvCol = 0;
-
-		switch (currentState->currentAction)
-		{
-		case Sprite_Action::STANDING:
-			if (animation->movAnimation.GetCurrentFrame() != 0)
-			{
-				animation->movAnimation.Update();
-			}
-			break;
-		case Sprite_Action::WALKING:
-			animation->movAnimation.Update();
-			break;
-		case Sprite_Action::RUNNING:
-			animation->movAnimation.Update();
-			break;
-		}
-
-		uvRow = currentAction;
-		uvCol = currentDirection + animation->movAnimation.GetCurrentFrame();
-
-		shader->use();
-
-		shader->setFloat("uvRow", (float)uvRow);
-		shader->setFloat("uvCol", (float)uvCol);
-
-	}
-}
-*/
+system_player_playAnimation::system_player_playAnimation(EntityComponentSystem& ecs) : ecs(ecs) {}
 
 void system_player_playAnimation::Update(float dt)
 {
@@ -104,10 +59,12 @@ void system_player_playAnimation::Update(float dt)
 
 		uvCol = static_cast<float>(animation->SpriteDirection) + animation->movAnimation.GetCurrentFrame();
 
-		shader->use();
+		shader->Bind();
 
 		shader->setFloat("uvRow", (float)uvRow);
 		shader->setFloat("uvCol", (float)uvCol);
+
+		shader->Unbind();
 
 	}
 }
