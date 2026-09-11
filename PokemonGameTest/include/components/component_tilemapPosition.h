8e7b6ec3feeaf90ac816b080552b58d94b3663cb
schall650 <oschall9016@gmail.com>
2026-09-10 18:26:51 -0600

    renamed currentMap to currentTileMap and added a pointer to the GameWorld

--- PokemonGameTest/include/components/component_tilemapPosition.h
+++ PokemonGameTest/include/components/component_tilemapPosition.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "Tilemap.h"
+#include "GameWorld.h"
 
 struct component_tilemapPosition
 {
 	float currentX = 0.0f;
 	float currentZ = 0.0f;
 
-	Tilemap* currentMap = nullptr;
+	Tilemap* currentTileMap = nullptr;
+	GameWorld* gameWorld = nullptr;
 };
