ef1445ad8b062302340d3e664c7a934b914f23c7
schall650 <oschall9016@gmail.com>
2026-09-10 18:25:59 -0600

    changed walk and run speed constants, added booleans for warping and inputlocked

--- PokemonGameTest/include/components/component_player_currentState.h
+++ PokemonGameTest/include/components/component_player_currentState.h
@@ -29,21 +29,26 @@ enum class Key_State
 };
 
 // time (in seconds) it takes to move one tile
-constexpr float WALK_SPEED = 0.4f; 
-constexpr float RUN_SPEED = 0.25f; 
+constexpr float WALK_SPEED = 0.3f; 
+constexpr float RUN_SPEED = 0.15f; 
 constexpr float BIKE_SPEED = 0.1f; 
 
 struct component_player_currentState
 {
+	// purely player input
 	Input_Action currentAction = Input_Action::STANDING;
 	Input_Direction currentDirection = Input_Direction::DOWN;
-
-	Input_Direction spriteDirection = Input_Direction::DOWN;
-
 	Key_State keyState = Key_State::RELEASED;
+
+	// actual sprite direction
+	Input_Direction spriteDirection = Input_Direction::DOWN;
 
 	bool moveRequest = false;
 	bool movementVisualsSynced = true;
 
+	bool inputLocked = false;
+
 	float moveSpeed = WALK_SPEED; 
+
+	bool warping = false;
 };
