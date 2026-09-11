5c68f2e5e5e321e37a907ccf0ca89cdc22da038e
schall650 <oschall9016@gmail.com>
2026-09-10 19:53:30 -0600

    removed use of holdTimer, added keyStates and use of the inputLocked bool from component_player_currentState

--- PokemonGameTest/src/systems/system_player_inputDetection.cpp
+++ PokemonGameTest/src/systems/system_player_inputDetection.cpp
@@ -21,84 +21,59 @@ void system_player_inputDetection::Update(float dt)
 	{
 		auto currentState = ecs.GetComponent<component_player_currentState>(entity);
 
-		float holdCooldown = 0; // move speed shouldn't go under this value or it stutters currentState->moveSpeed / 2
+		if (currentState->inputLocked)
+		{
+			continue;
+		}
 
 		// input is only pressed, update immediately 
 		if (input.isKeyPressed(SDL_SCANCODE_W))
 		{
-			updateComponent(currentState, Input_Direction::UP);
-			currentState->keyState = Key_State::PRESSED;
+			updateComponent(currentState, Input_Direction::UP, Key_State::PRESSED);
 		}
 		else if (input.isKeyPressed(SDL_SCANCODE_S))
 		{
-			updateComponent(currentState, Input_Direction::DOWN);
-			currentState->keyState = Key_State::PRESSED;
+			updateComponent(currentState, Input_Direction::DOWN, Key_State::PRESSED);
 		}
 		else if (input.isKeyPressed(SDL_SCANCODE_A))
 		{
-			updateComponent(currentState, Input_Direction::LEFT);
-			currentState->keyState = Key_State::PRESSED;
+			updateComponent(currentState, Input_Direction::LEFT, Key_State::PRESSED);
 		}
 		else if (input.isKeyPressed(SDL_SCANCODE_D))
 		{
-			updateComponent(currentState, Input_Direction::RIGHT);
-			currentState->keyState = Key_State::PRESSED;
+			updateComponent(currentState, Input_Direction::RIGHT, Key_State::PRESSED);
 		}
 
 		// input is being held
-		// only update after a small delay (prevents double stepping on press)
-		// and if the movement visuals are synced (prevents moving one tile too far)
 		else if (input.isKeyHeld(SDL_SCANCODE_W))
 		{
-			currentState->keyState = Key_State::HELD;
-			if (holdTimer >= holdCooldown && currentState->movementVisualsSynced)
-			{
-				updateComponent(currentState, Input_Direction::UP);
-				holdTimer = 0.0f;
-			}
-			else holdTimer += dt;
+			updateComponent(currentState, Input_Direction::UP, Key_State::HELD);
 		}
 		else if (input.isKeyHeld(SDL_SCANCODE_S))
 		{
-			currentState->keyState = Key_State::HELD;
-			if (holdTimer >= holdCooldown && currentState->movementVisualsSynced)
-			{
-				updateComponent(currentState, Input_Direction::DOWN);
-				holdTimer = 0.0f;
-			}
-			else holdTimer += dt;
+			updateComponent(currentState, Input_Direction::DOWN, Key_State::HELD);
 		}
 		else if (input.isKeyHeld(SDL_SCANCODE_A))
 		{
-			currentState->keyState = Key_State::HELD;
-			if (holdTimer >= holdCooldown && currentState->movementVisualsSynced)
-			{
-				updateComponent(currentState, Input_Direction::LEFT);
-				holdTimer = 0.0f;
-			}
-			else holdTimer += dt;
+			updateComponent(currentState, Input_Direction::LEFT, Key_State::HELD);
 		}
 		else if (input.isKeyHeld(SDL_SCANCODE_D))
 		{
-			currentState->keyState = Key_State::HELD;
-			if (holdTimer >= holdCooldown && currentState->movementVisualsSynced)
-			{
-				updateComponent(currentState, Input_Direction::RIGHT);
-				holdTimer = 0.0f;
-			}
-			else holdTimer += dt;
+			updateComponent(currentState, Input_Direction::RIGHT, Key_State::HELD);
 		}
+
+		// no input
 		else
 		{
-			holdTimer = 0.0f;
 			currentState->currentAction = Input_Action::STANDING;
 			currentState->keyState = Key_State::RELEASED;
 		}
 	}
 }
 
-void system_player_inputDetection::updateComponent(component_player_currentState* state, Input_Direction direction)
+void system_player_inputDetection::updateComponent(component_player_currentState* state, Input_Direction direction, Key_State keyState)
 {
+	// player is running if shift is held
 	if (input.isKeyPressed(SDL_SCANCODE_LSHIFT) || input.isKeyHeld(SDL_SCANCODE_LSHIFT))
 	{
 		state->currentAction = Input_Action::RUNNING;
@@ -110,6 +85,10 @@ void system_player_inputDetection::updateComponent(component_player_currentState
 		state->moveSpeed = WALK_SPEED;
 	}
 
-	state->moveRequest = true;
 	state->currentDirection = direction;
+	state->keyState = keyState;
+
+	// attempting to move
+	state->moveRequest = true;
+	
 }
