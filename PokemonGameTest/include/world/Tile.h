d82624ba936fd015efb0db7befd6106ae8c09ec7
schall650 <oschall9016@gmail.com>
2026-09-10 18:29:57 -0600

    added a warp TileSignature and warpIndex to each tile

--- PokemonGameTest/include/world/Tile.h
+++ PokemonGameTest/include/world/Tile.h
@@ -1,15 +1,20 @@
 #pragma once
 
-constexpr int TILE_SIZE = 32;
+class GameMap;
+
+constexpr int TILE_SIZE = 16;
+constexpr int NO_WARP = -1;
 
 // dictates tile behaivor
 enum TileSignature : char
 {
 	WALKABLE = 1 << 0,
-	COLLIDER = 1 << 1
+	COLLIDER = 1 << 1,
+	WARP = 1 << 2
 };
 
 struct Tile
 {
 	TileSignature signature = WALKABLE;
+	int warpIndex = NO_WARP;
 };
