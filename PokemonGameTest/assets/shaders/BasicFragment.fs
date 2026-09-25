22bddfbd6b96d1119b08c8f0c06293c4c26160da
schall650 <oschall9016@gmail.com>
2026-09-25 15:21:48 -0600

    added grad shader for tile visualization

--- PokemonGameTest/assets/shaders/BasicFragment.fs
+++ PokemonGameTest/assets/shaders/BasicFragment.fs
@@ -1,8 +1,16 @@
 #version 330 core
 
 out vec4 FragColor;
+in vec2 TexCoords;
+
+uniform float x;
+uniform float z;
 
 void main()
 {
-   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+
+    if (mod(x + z, 2.0) == 0.0)
+        FragColor = vec4(1,0.5,0.2,1);
+    else
+        FragColor = vec4(0.7,0.7,0.7,0.7);
 }
