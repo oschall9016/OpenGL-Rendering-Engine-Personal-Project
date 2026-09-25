78c4f37f49325c5cde094bef1732005b89fdc3d7
schall650 <oschall9016@gmail.com>
2026-09-25 15:29:43 -0600

    changed Tile_Signature and the struct signature to uint8_t

--- PokemonGameTest/include/world/Tile.h
+++ PokemonGameTest/include/world/Tile.h
@@ -1,20 +1,23 @@
 #pragma once
 
+#include <cstdint>
+
 class GameMap;
 
-constexpr int TILE_SIZE = 16;
+constexpr int TILE_SIZE = 16; // 16 pixels, 1 world unit
 constexpr int NO_WARP = -1;
 
 // dictates tile behaivor
-enum TileSignature : char
+enum Tile_Type : uint8_t
 {
 	WALKABLE = 1 << 0,
 	COLLIDER = 1 << 1,
-	WARP = 1 << 2
+	WARP = 1 << 2,
+	SLOPE = 1 << 3
 };
 
 struct Tile
 {
-	TileSignature signature = WALKABLE;
-	int warpIndex = NO_WARP;
+	uint16_t warpIndex = NO_WARP;
+	uint8_t signature = Tile_Type::WALKABLE;
 };
